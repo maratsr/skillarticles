4a8f2c0d70484fced26f9a81d5dbeb0a7824f26c
Marat R. Sadykov <sadykovmr@tatsotsbank.ru>
2020-02-11 17:23:23 +0300

    hometask 4 - Change SharedPreferences source

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -2,9 +2,10 @@ package ru.skillbranch.skillarticles.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
+
 import androidx.preference.PreferenceManager
 
 class PrefManager(context: Context) {
-    val preferences: SharedPreferences  = PreferenceManager(context).sharedPreferences
+    val preferences: SharedPreferences = PreferenceManager(context).sharedPreferences
     fun clearAll() = preferences.edit().clear().apply()
 }

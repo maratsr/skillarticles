a2c025792fdd0942e44a75be9c6014dc920dcaf1
Marat R. Sadykov <sadykovmr@tatsotsbank.ru>
2020-07-10 11:35:05 +0300

    lesson 10 (room) hometasks

--- app/src/main/java/ru/skillbranch/skillarticles/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/profile/ProfileFragment.kt
@@ -1,11 +1,11 @@
 package ru.skillbranch.skillarticles.ui.profile
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.viewmodels.profile.ProfileViewModel
@@ -27,7 +27,7 @@ class ProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         // TODO: Use the ViewModel
     }
 

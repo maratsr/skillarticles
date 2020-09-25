4e3ec3aa38e24945bce21935d163697a412e5345
Marat R. Sadykov <sadykovmr@tatsotsbank.ru>
2020-09-25 11:17:51 +0300

    registration - fix POST instead of PUT etc

--- app/src/main/java/ru/skillbranch/skillarticles/data/remote/req/LoginReq.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/remote/req/LoginReq.kt
@@ -1,3 +1,4 @@
 package ru.skillbranch.skillarticles.data.remote.req
 
 data class LoginReq(val login:String, val password:String)
+data class SignUpReq(val name: String, val email: String, val password: String)
--- app/src/main/java/ru/skillbranch/skillarticles/data/remote/req/SignUpReq.kt
+++
@@ -1,3 +0,0 @@
-package ru.skillbranch.skillarticles.data.remote.req
-
-data class SignUpReq(val name: String, val email: String, val password: String)
